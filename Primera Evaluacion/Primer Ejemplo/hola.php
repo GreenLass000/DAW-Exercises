f4d6ef5995f8f70f9d4068b4f98889e7c513bcdd
= <=>
2023-09-25 13:00:37 +0200

    Tema 1 Primer ejemplo

---
+++ Primera Evaluacion/Primer Ejemplo/hola.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo</title>
+</head>
+
+<body>
+
+    <body>
+
+        <h1>Primer ejemplo: </h1>
+
+        <!-- El navegador descarga el archivo PHP -->
+        <?php
+        echo "Hola mundo.";
+        ?>
+
+    </body>
+
+</body>
+
+</html>
