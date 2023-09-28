13921eddfc44af51a250c19d7fcd97e77b56fcc7
= <=>
2023-09-28 11:33:36 +0200

    Reorden de carpetas + ejemplos

---
+++ Primera Evaluacion/Tema1/Ejemlpos/Ej2-VariablesNoInicializacion/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $n1 = 100;
+    $n3 = 100 + $n2;
+    echo "$n3 <br>";
+    $n3 = 100 * $n2;
+    echo "$n3 <br>";
+    ?>
+</body>
+
+</html>
