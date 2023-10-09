54119749337bc399aeeb89a62c1d79083b2be67c
= <=>
2023-10-09 09:59:59 +0200

    Tema 3 ejercicio 2

--- Primera Evaluacion/Tema3/Ejercicio2/js/Figura.js
+++ Primera Evaluacion/Tema3/Ejercicio2/js/Figura.js
@@ -1,7 +1,14 @@
-function Figura(x, y) {
+function Figura(x, y, color, tipo) {
     this.x = x;
     this.y = y;
+    this.color = color;
+    this.tipo = tipo;
     this.moverA = function (newX, newY) {
-        
+        this.x = newX;
+        this.y = newY;
     };
+}
+
+Figura.prototype.toString = function () {
+    return `${this.tipo} ${this.color}`;
 }
---
+++ Primera Evaluacion/Tema3/Ejercicio2/js/script.js
@@ -0,0 +1,79 @@
+let tablero = [];
+const FIGURASW = [];
+const FIGURASB = [];
+
+genTablero();
+
+fillPiezas(FIGURASW, 8, "P", "White");
+fillPiezas(FIGURASW, 2, "T", "White");
+fillPiezas(FIGURASW, 2, "C", "White");
+fillPiezas(FIGURASW, 2, "A", "White");
+fillPiezas(FIGURASW, 1, "Q", "White");
+fillPiezas(FIGURASW, 1, "K", "White");
+
+fillPiezas(FIGURASB, 8, "P", "Black");
+fillPiezas(FIGURASB, 2, "T", "Black");
+fillPiezas(FIGURASB, 2, "C", "Black");
+fillPiezas(FIGURASB, 2, "A", "Black");
+fillPiezas(FIGURASB, 1, "Q", "Black");
+fillPiezas(FIGURASB, 1, "K", "Black");
+
+colocarPiezas(FIGURASW, tablero);
+colocarPiezas(FIGURASB, tablero);
+
+function colocarPieza(figura, tablero) {
+
+    let x = ~~(Math.random() * 8);
+    let y = ~~(Math.random() * 8);
+
+    figura.moverA(x, y);
+
+    let posX = figura.x;
+    let posY = figura.y;
+
+    if (tablero[posY][posX] === 0) {
+        tablero[posY][posX] = figura.toString();
+    } else {
+        colocarPieza(figura, tablero);
+    }
+
+    /*tablero[posY][posX] = tablero[posY][posX] === 0 ?
+        figura.toString() : colocarPieza(figura, tablero);*/
+}
+
+function colocarPiezas(figuras, tablero) {
+    for (let figura of figuras) {
+        let posX = figura.x;
+        let posY = figura.y;
+        //console.log(figura.toString(), posX, posY);
+
+        if (tablero[posY][posX] === 0) {
+            tablero[posY][posX] = figura.toString();
+        } else {
+            //console.log("Ocupado", figura);
+            colocarPieza(figura, tablero);
+        }
+
+        /*tablero[posY][posX] = tablero[posY][posX] === 0 ?
+            figura.toString() : colocarPieza(figura, tablero);*/
+    }
+}
+
+function fillPiezas(arr, number, type, color) {
+    for (let i = 0; i < number; i++) {
+        let x = ~~(Math.random() * 8);
+        let y = ~~(Math.random() * 8);
+        arr.push(new Figura(x, y, color, type));
+    }
+}
+
+function genTablero() {
+    for (let i = 0; i < 8; i++) {
+        tablero[i] = [];
+        for (let j = 0; j < 8; j++) {
+            tablero[i][j] = 0;
+        }
+    }
+}
+
+console.table(tablero);
