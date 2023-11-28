568dc04ed281754295a5e786d2a84b348aa1f91f
= <=>
2023-11-28 10:55:23 +0100

    Tema 6 Ejercicios

--- Segunda Evaluacion/Tema6/Ejercicios/Ejercicio1/js/script.js
+++ Segunda Evaluacion/Tema6/Ejercicios/Ejercicio1/js/script.js
@@ -3,9 +3,25 @@ xhr.addEventListener("load", () => {
     if (xhr.status === 200) {
         const flags = document.querySelector("#flags");
         const flag = document.createElement("div");
+        flag.classList.add("flag");
+        const name = document.createElement("div");
+        name.classList.add("name");
+        const img = document.createElement("img");
+        img.classList.add("img");
 
         xhr.response.forEach(d => {
-            console.log(d.name["common"], d.flags["png"]);
+
+            const cloneName = name.cloneNode();
+            const cloneImg = img.cloneNode();
+            const cloneFlag = flag.cloneNode();
+
+            cloneName.textContent = d.name["common"];
+            cloneImg.src = d.flags["svg"];
+
+            cloneFlag.appendChild(cloneName);
+            cloneFlag.appendChild(cloneImg);
+
+            flags.appendChild(cloneFlag);
         });
 
     } else {
