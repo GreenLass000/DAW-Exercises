e4f9ca6da01a0b3c532dd69cee66286ac6b60203
= <=>
2024-02-22 09:55:36 +0100

    Anglar

---
+++ Angular/kodo/src/app/recomended/recomended.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recomended',
+  standalone: true,
+  imports: [],
+  templateUrl: './recomended.component.html',
+  styleUrl: './recomended.component.css'
+})
+export class RecomendedComponent {
+
+}
